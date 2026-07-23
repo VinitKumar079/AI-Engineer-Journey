bfcf1220b63deb2afb882c9ff185ad0998ee911c
VinitKumar079 <vinitkumar2020079@gmail.com>
2026-07-23 16:05:23 +0530

    Day 1 - Python Basics

---
+++ day1.py
@@ -0,0 +1,17 @@
+print("Welcome to AI Engineer Journey!")
+
+name = "Vinit"
+age = 20
+
+print(name)
+print(age)
+
+a = 10
+b = 20
+
+print(a + b)
+print(a * b)
+
+name = input("Enter your name: ")
+
+print("Welcome", name)
