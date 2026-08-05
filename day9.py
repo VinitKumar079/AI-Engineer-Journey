051f3e66f1de16e7b2841c77de6b6b422a147c3c
VinitKumar079 <vinitkumar2020079@gmail.com>
2026-08-05 17:48:01 +0530

    Day 9 - Modules and Built-in Modules

---
+++ day9.py
@@ -0,0 +1,24 @@
+# ==========================================
+# AI Engineer Journey
+# Official Day 9
+# Module Practice
+# ==========================================
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+
+    return a / b
